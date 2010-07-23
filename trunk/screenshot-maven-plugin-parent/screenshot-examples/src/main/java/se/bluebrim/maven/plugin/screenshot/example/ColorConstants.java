3d38dc6e47a3e12c8028c79d43615dd09b1e8b38
goran.stack <goran.stack@665c6c9b-6b17-18b1-5103-0a9df7e29ab1>
2010-07-23 13:20:46 +0000

    Added the new screenshot-examples module

---
+++ trunk/screenshot-maven-plugin-parent/screenshot-examples/src/main/java/se/bluebrim/maven/plugin/screenshot/example/ColorConstants.java
@@ -0,0 +1,89 @@
+package se.bluebrim.maven.plugin.screenshot.example;
+
+import java.awt.Color;
+
+/**
+ * <img src="doc-files/ColorConstants.png">
+ * <p>
+ * This is an example of a resource class with visual samples included in the Javadoc.
+ * The images for the Javadoc are created by a few lines of  code in a test class.
+ * The test class is processed by the screenshot-maven-plugin to produce the images.
+ * </p>
+ * 
+ * @author Goran Stack
+ *
+ */
+public class ColorConstants {
+	
+	/**
+	 * <img src="doc-files/ColorConstants-white.png">
+	 */
+    public final static Color WHITE = Color.WHITE;
+
+	/**
+	 * <img src="doc-files/ColorConstants-light_gray.png">
+	 */
+    public final static Color LIGHT_GRAY = Color.LIGHT_GRAY;
+
+	/**
+	 * <img src="doc-files/ColorConstants-gray.png">
+	 */
+    public final static Color GRAY = Color.GRAY;
+
+    /**
+	 * <img src="doc-files/ColorConstants-dark_gray.png">
+	 */
+    public final static Color DARK_GRAY = Color.DARK_GRAY;
+
+	/**
+	 * <img src="doc-files/ColorConstants-black.png">
+	 */
+    public final static Color BLACK = Color.BLACK;
+    
+	/**
+	 * <img src="doc-files/ColorConstants-red.png">
+	 */
+    public final static Color RED = Color.RED;
+
+	/**
+	 * <img src="doc-files/ColorConstants-pink.png">
+	 */
+    public final static Color PINK = Color.PINK;
+
+	/**
+	 * <img src="doc-files/ColorConstants-orange.png">
+	 */
+    public final static Color ORANGE = Color.ORANGE;
+
+	/**
+	 * <img src="doc-files/ColorConstants-yellow.png">
+	 */
+    public final static Color YELLOW = Color.YELLOW;
+
+	/**
+	 * <img src="doc-files/ColorConstants-green.png">
+	 */
+    public final static Color GREEN = Color.GREEN;
+
+	/**
+	 * <img src="doc-files/ColorConstants-magenta.png">
+	 */
+    public final static Color MAGENTA = Color.MAGENTA;
+
+	/**
+	 * <img src="doc-files/ColorConstants-cyan.png">
+	 */
+    public final static Color CYAN = Color.CYAN;
+
+	/**
+	 * <img src="doc-files/ColorConstants-blue.png">
+	 */
+    public final static Color BLUE = Color.BLUE;
+    
+	/**
+	 * <img src="doc-files/ColorConstants-transparent_blue.png">
+	 */
+    public final static Color TRANSPARENT_BLUE = new Color(0f, 0f, 1f, 0.5f);
+
+
+}
