d6a4581842323eeb2e59c32112dcc1a986c2d82f
goran.stack <goran.stack@665c6c9b-6b17-18b1-5103-0a9df7e29ab1>
2010-08-02 11:10:24 +0000

    Improved screenshot layout

--- screenshot-maven-plugin-parent/screenshot-examples/src/test/java/se/bluebrim/maven/plugin/screenshot/example/ColorConstantsTest.java
+++ screenshot-maven-plugin-parent/screenshot-examples/src/test/java/se/bluebrim/maven/plugin/screenshot/example/ColorConstantsTest.java
@@ -3,6 +3,9 @@ package se.bluebrim.maven.plugin.screenshot.example;
 import java.util.Collection;
 
 import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+
+import org.jdesktop.swingx.JXFrame;
 
 import se.bluebrim.maven.plugin.screenshot.Screenshot;
 import se.bluebrim.maven.plugin.screenshot.ScreenshotDescriptor;
@@ -21,6 +24,25 @@ import se.bluebrim.maven.plugin.screenshot.sample.SampleUtil;
  *
  */
 public class ColorConstantsTest {
+
+	public static void main(String[] args) {
+		final ColorConstantsTest instance = new ColorConstantsTest();
+		SwingUtilities.invokeLater(new Runnable() {			
+			@Override
+			public void run() {
+				instance.openInWindow();			
+			}
+		});
+	}
+
+	private void openInWindow()
+	{
+		JXFrame window = new JXFrame(getClass().getSimpleName(), true);
+		window.getContentPane().add(createPaletteScreenshot());
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);		
+	}	
 
 
 	@Screenshot(oneForEachLocale = false, targetClass = ColorConstants.class)
